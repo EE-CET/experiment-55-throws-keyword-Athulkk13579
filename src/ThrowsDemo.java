be1fee544ed3b6f79184d8d7c98a07a58cc66f5c
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-02-20 10:27:27 +0530

    Update ThrowsDemo.java

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
-import java.io.IOException;
 
-public class ThrowsDemo {
-    
-    // TODO: Define static method check(int n) that throws IOException
-    // If n < 0, throw new IOException()
-    // Else, print n
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
 
-   
-        // TODO: Read n
-        
-        // TODO: try {
-        //     call check(n)
-        // } catch (IOException e) {
-        //     Print "Caught: IO Exception"
-        // }
-    
+public class AgeValidation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            int age = scanner.nextInt();
+
+            try {
+                if (age < 18) {
+                    throw new InvalidAgeException("Not eligible to vote");
+                } else {
+                    System.out.println("Eligible to vote");
+                }
+            } catch (InvalidAgeException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        scanner.close();
+    }
 }
